20fbdc41d3f907de96317e855849aa9e8943ea8f
Joshua Shanks <jjshanks@gmail.com>
2013-11-09 05:39:14 +0000

    update dweight for excersize 2.3

--- modern/dweight.c
+++ modern/dweight.c
@@ -3,19 +3,13 @@
 #include <stdio.h>
 
 int main(void) {
-    int height, length, width, volume, weight;
-
-    height = 8;
-    length = 12;
-    width = 10;
+    int volume, height = 8, length = 12, width = 10;
 
     volume = height * length * width;
 
-    weight = (volume + 165) / 166;
-
     printf("Dimensions: %dx%dx%d\n", length, width, height);
     printf("Volume (cubic inches): %d\n", volume);
-    printf("Dimensional weight (pounds): %d\n", weight);
+    printf("Dimensional weight (pounds): %d\n", (volume + 165) / 166);
 
     return 0;
 }
